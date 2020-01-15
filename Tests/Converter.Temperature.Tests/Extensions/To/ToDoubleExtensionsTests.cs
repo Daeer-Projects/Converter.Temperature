b9d4bb8cbf0af28a3a6156e56fb41b3d3b3355a0
David Clark <david.clark2@capita.co.uk>
2020-01-15 14:03:23 +0000

    Added more to the gas and celsius tests to catch the gas edge cases. Increased the code coverage.

--- Tests/Converter.Temperature.Tests/Extensions/To/ToDoubleExtensionsTests.cs
+++ Tests/Converter.Temperature.Tests/Extensions/To/ToDoubleExtensionsTests.cs
@@ -37,15 +37,36 @@ namespace Converter.Temperature.Tests.Extensions.To
             result.Should().Be(expected);
         }
 
-        [Fact]
-        public void Test_to_celsius_from_gas_returns_correct_value()
+        [Theory]
+        [InlineData(0.25d, 125d)]
+        [InlineData(0.9d, 125d)]
+        [InlineData(1d, 140d)]
+        [InlineData(1.4d, 140d)]
+        [InlineData(1.5d, 150d)]
+        [InlineData(2.4d, 150d)]
+        [InlineData(2.5d, 165d)]
+        [InlineData(3.4d, 165d)]
+        [InlineData(3.5d, 180d)]
+        [InlineData(4.4d, 180d)]
+        [InlineData(4.5d, 190d)]
+        [InlineData(5.4d, 190d)]
+        [InlineData(5.5d, 200d)]
+        [InlineData(6.4d, 200d)]
+        [InlineData(6.5d, 220d)]
+        [InlineData(7.4d, 220d)]
+        [InlineData(7.5d, 230d)]
+        [InlineData(8.4d, 230d)]
+        [InlineData(8.5d, 240d)]
+        [InlineData(9.4d, 240d)]
+        [InlineData(9.5d, 260d)]
+        [InlineData(9.9d, 260d)]
+        public void Test_to_celsius_from_gas_returns_correct_value(double input, double expected)
         {
             // Arrange.
-            const double expected = 220d;
-            var input = new GasDouble(7);
+            var inputGas = new GasDouble(input);
 
             // Act.
-            var result = input.ToCelsius();
+            var result = inputGas.ToCelsius();
 
             // Assert.
             result.Should().Be(expected);
@@ -120,15 +141,37 @@ namespace Converter.Temperature.Tests.Extensions.To
             result.Should().Be(expected);
         }
         
-        [Fact]
-        public void Test_to_gas_from_celsius_returns_correct_value()
+        [Theory]
+        [InlineData(80, 0.25d)]
+        [InlineData(114, 0.25d)]
+        [InlineData(115, 0.5d)]
+        [InlineData(134, 0.5d)]
+        [InlineData(135, 1d)]
+        [InlineData(144, 1d)]
+        [InlineData(145, 2d)]
+        [InlineData(154, 2d)]
+        [InlineData(155, 3d)]
+        [InlineData(174, 3d)]
+        [InlineData(175, 4d)]
+        [InlineData(184, 4d)]
+        [InlineData(185, 5d)]
+        [InlineData(194, 5d)]
+        [InlineData(195, 6d)]
+        [InlineData(209, 6d)]
+        [InlineData(210, 7d)]
+        [InlineData(224, 7d)]
+        [InlineData(225, 8d)]
+        [InlineData(234, 8d)]
+        [InlineData(235, 9d)]
+        [InlineData(244, 9d)]
+        [InlineData(245, 10d)]
+        [InlineData(269, 10d)]
+        public void Test_to_gas_from_celsius_returns_correct_value(double input, double expected)
         {
             // Arrange.
-            const double expected = 6d;
-            var input = new CelsiusDouble(200);
-
+            var inputCelsius = new CelsiusDouble(input);
             // Act.
-            var result = input.ToGas();
+            var result = inputCelsius.ToGas();
 
             // Assert.
             result.Should().Be(expected);
