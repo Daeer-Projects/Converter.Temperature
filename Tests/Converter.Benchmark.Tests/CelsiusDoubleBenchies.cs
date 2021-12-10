dfbdb8e62cd6a4f4737a35cbd71e002be71ac5f6
David Clark <krennic51@gmail.com>
2021-12-10 20:09:34 +0000

    Add benchmark project
    Added the first of many benchmark classes.

---
+++ Tests/Converter.Benchmark.Tests/CelsiusDoubleBenchies.cs
@@ -0,0 +1,27 @@
+﻿namespace Converter.Benchmark.Tests
+{
+    using BenchmarkDotNet.Attributes;
+    using Temperature.Extensions.From;
+    using Temperature.Extensions.To;
+    using Temperature.TemperatureTypes;
+
+    [MemoryDiagnoser]
+    public class CelsiusDoubleBenchies
+    {
+        [Benchmark]
+        public double ConvertFromCelsiusToFahrenheit()
+        {
+            const double input = 42.4756843d;
+            double result = input.FromCelsius().ToFahrenheit();
+            return result;
+        }
+
+        [Benchmark]
+        public double ConvertFromCelsiusToFahrenheitGeneric()
+        {
+            const double input = 42.4756843d;
+            double result = input.From<Celsius>().To<Fahrenheit>();
+            return result;
+        }
+    }
+}
---
+++ Tests/Converter.Benchmark.Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Converter.Benchmark.Tests
+{
+    using System;
+    using BenchmarkDotNet.Running;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Running Benchmarks...");
+
+            var summary = BenchmarkRunner.Run<CelsiusDoubleBenchies>();
+        }
+    }
+}
