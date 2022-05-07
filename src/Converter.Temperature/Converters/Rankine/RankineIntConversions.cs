e4bfc563cb209f24d0524811ea9c93a38f0f51be
David Clark <krennic51@gmail.com>
2022-05-07 15:37:31 +0100

    Add Rankine conversions

---
+++ src/Converter.Temperature/Converters/Rankine/RankineIntConversions.cs
@@ -0,0 +1,77 @@
+﻿namespace Converter.Temperature.Converters.Rankine
+{
+    using Celsius;
+
+    internal static class RankineIntConversions
+    {
+        /// <summary>
+        /// The rankine to rankine conversion.
+        /// </summary>
+        /// <param name="input"> The temperature to convert. </param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+        /// <returns>
+        /// The converted temperature.
+        /// </returns>
+        public static int RankineToRankine(int input)
+        {
+            return input;
+        }
+
+        /// <summary>
+        /// The rankine to celsius conversion.
+        /// </summary>
+        /// <param name="input"> The temperature to convert. </param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+        /// <returns>
+        /// The converted temperature.
+        /// </returns>
+        public static int RankineToCelsius(int input)
+        {
+            var rankineTemp = (input - 492) * 5 / 9;
+            return rankineTemp;
+        }
+
+        /// <summary>
+        /// The rankine to fahrenheit conversion.
+        /// </summary>
+        /// <param name="input"> The temperature to convert. </param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+        /// <returns>
+        /// The converted temperature.
+        /// </returns>
+        public static int RankineToFahrenheit(int input)
+        {
+            var rankineTemp = input - 460;
+            return rankineTemp;
+        }
+
+        /// <summary>
+        /// The rankine to kelvin conversion.
+        /// </summary>
+        /// <param name="input"> The temperature to convert. </param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+        /// <returns>
+        /// The converted temperature.
+        /// </returns>
+        public static int RankineToKelvin(int input)
+        {
+            var rankineTemp = input * 5 / 9;
+            return rankineTemp;
+        }
+
+        /// <summary>
+        /// The rankine to gas conversion.
+        /// </summary>
+        /// <param name="input"> The temperature to convert. </param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+        /// <returns>
+        /// The converted temperature.
+        /// </returns>
+        public static int RankineToGas(int input)
+        {
+            var celsiusTemp = RankineToCelsius(input);
+            var gasTemp = CelsiusIntConversions.CelsiusToGas(celsiusTemp);
+            return gasTemp;
+        }
+    }
+}
---
+++ src/Converter.Temperature/Converters/Rankine/RankineLongConversions.cs
@@ -0,0 +1,77 @@
+﻿namespace Converter.Temperature.Converters.Rankine
+{
+    using Celsius;
+
+    internal static class RankineLongConversions
+    {
+        /// <summary>
+        /// The rankine to rankine conversion.
+        /// </summary>
+        /// <param name="input"> The temperature to convert. </param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+        /// <returns>
+        /// The converted temperature.
+        /// </returns>
+        public static long RankineToRankine(long input)
+        {
+            return input;
+        }
+
+        /// <summary>
+        /// The rankine to celsius conversion.
+        /// </summary>
+        /// <param name="input"> The temperature to convert. </param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+        /// <returns>
+        /// The converted temperature.
+        /// </returns>
+        public static long RankineToCelsius(long input)
+        {
+            var rankineTemp = (input - 492L) * 5 / 9;
+            return rankineTemp;
+        }
+
+        /// <summary>
+        /// The rankine to fahrenheit conversion.
+        /// </summary>
+        /// <param name="input"> The temperature to convert. </param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+        /// <returns>
+        /// The converted temperature.
+        /// </returns>
+        public static long RankineToFahrenheit(long input)
+        {
+            var rankineTemp = input - 460L;
+            return rankineTemp;
+        }
+
+        /// <summary>
+        /// The rankine to kelvin conversion.
+        /// </summary>
+        /// <param name="input"> The temperature to convert. </param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+        /// <returns>
+        /// The converted temperature.
+        /// </returns>
+        public static long RankineToKelvin(long input)
+        {
+            var rankineTemp = input * 5 / 9;
+            return rankineTemp;
+        }
+
+        /// <summary>
+        /// The rankine to gas conversion.
+        /// </summary>
+        /// <param name="input"> The temperature to convert. </param>
+        /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+        /// <returns>
+        /// The converted temperature.
+        /// </returns>
+        public static long RankineToGas(long input)
+        {
+            var celsiusTemp = RankineToCelsius(input);
+            var gasTemp = CelsiusLongConversions.CelsiusToGas(celsiusTemp);
+            return gasTemp;
+        }
+    }
+}
