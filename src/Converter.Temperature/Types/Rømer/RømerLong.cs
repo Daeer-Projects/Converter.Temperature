d296fbc060acbf7d21ae57fab66d03fb1d8e0a97
David Clark <krennic51@gmail.com>
2023-02-19 15:01:08 +0000

    Add Rømer type with spelling override
    Added the base Rømer types for conversions.
    
    # Conflicts:
    #	Converter.Temperature.sln.DotSettings

---
+++ src/Converter.Temperature/Types/Rømer/RømerLong.cs
@@ -0,0 +1,13 @@
+﻿namespace Converter.Temperature.Types.Rømer;
+
+using BaseTypes;
+
+public sealed class RømerLong : LongBase
+{
+    public RømerLong(long temp)
+    {
+        Temperature = temp;
+    }
+
+    public long Temperature { get; }
+}
