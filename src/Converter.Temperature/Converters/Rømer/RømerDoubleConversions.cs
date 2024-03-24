63ef46ff3eb4d7e671295c3546c235bbff99de5f
David Clark <krennic51@gmail.com>
2024-03-24 14:34:42 +0000

    Add empty Rømer conversion types

---
+++ src/Converter.Temperature/Converters/Rømer/RømerDoubleConversions.cs
@@ -0,0 +1,80 @@
+﻿namespace Converter.Temperature.Converters.Rømer;
+
+internal static class RømerDoubleConversions
+{
+    /// <summary>
+    ///     The rømer to celsius conversion.
+    /// </summary>
+    /// <param name="input"> The temperature to convert. </param>
+    /// <returns>
+    ///     The converted temperature.
+    /// </returns>
+    public static double RømerToCelsius(double input)
+    {
+        return 0.0d;
+    }
+
+    /// <summary>
+    ///     The rømer to fahrenheit conversion.
+    /// </summary>
+    /// <param name="input"> The temperature to convert. </param>
+    /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+    /// <returns>
+    ///     The converted temperature.
+    /// </returns>
+    public static double RømerToFahrenheit(double input)
+    {
+        return 0.0d;
+    }
+
+    /// <summary>
+    ///     The rømer to kelvin conversion.
+    /// </summary>
+    /// <param name="input"> The temperature to convert. </param>
+    /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+    /// <returns>
+    ///     The converted temperature.
+    /// </returns>
+    public static double RømerToKelvin(double input)
+    {
+        return 0.0d;
+    }
+
+    /// <summary>
+    ///     The rømer to gas conversion.
+    /// </summary>
+    /// <param name="input"> The temperature to convert. </param>
+    /// <exception cref="T:System.ArgumentOutOfRangeException"> Temp too low or too high for gas mark! </exception>
+    /// <returns>
+    ///     The converted temperature.
+    /// </returns>
+    public static double RømerToGas(double input)
+    {
+        return 0.0d;
+    }
+
+    /// <summary>
+    ///     The rømer to rankine conversion.
+    /// </summary>
+    /// <param name="input"> The temperature to convert. </param>
+    /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+    /// <returns>
+    ///     The converted temperature.
+    /// </returns>
+    public static double RømerToRankine(double input)
+    {
+        return 0.0d;
+    }
+
+    /// <summary>
+    ///     The rømer to rømer conversion.
+    /// </summary>
+    /// <param name="input"> The temperature to convert. </param>
+    /// <returns>
+    ///     The converted temperature.
+    /// </returns>
+    public static double RømerToRømer(double input)
+    {
+        return input;
+    }
+}
---
+++ src/Converter.Temperature/Converters/Rømer/RømerLongConversions.cs
@@ -0,0 +1,80 @@
+﻿namespace Converter.Temperature.Converters.Rømer;
+
+internal static class RømerLongConversions
+{
+    /// <summary>
+    ///     The rømer to celsius conversion.
+    /// </summary>
+    /// <param name="input"> The temperature to convert. </param>
+    /// <returns>
+    ///     The converted temperature.
+    /// </returns>
+    public static long RømerToCelsius(long input)
+    {
+        return 0L;
+    }
+
+    /// <summary>
+    ///     The rømer to fahrenheit conversion.
+    /// </summary>
+    /// <param name="input"> The temperature to convert. </param>
+    /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+    /// <returns>
+    ///     The converted temperature.
+    /// </returns>
+    public static long RømerToFahrenheit(long input)
+    {
+        return 0L;
+    }
+
+    /// <summary>
+    ///     The rømer to kelvin conversion.
+    /// </summary>
+    /// <param name="input"> The temperature to convert. </param>
+    /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+    /// <returns>
+    ///     The converted temperature.
+    /// </returns>
+    public static long RømerToKelvin(long input)
+    {
+        return 0L;
+    }
+
+    /// <summary>
+    ///     The rømer to gas conversion.
+    /// </summary>
+    /// <param name="input"> The temperature to convert. </param>
+    /// <exception cref="T:System.ArgumentOutOfRangeException"> Temp too low or too high for gas mark! </exception>
+    /// <returns>
+    ///     The converted temperature.
+    /// </returns>
+    public static long RømerToGas(long input)
+    {
+        return 0L;
+    }
+
+    /// <summary>
+    ///     The rømer to rankine conversion.
+    /// </summary>
+    /// <param name="input"> The temperature to convert. </param>
+    /// <exception cref="T:System.ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+    /// <returns>
+    ///     The converted temperature.
+    /// </returns>
+    public static long RømerToRankine(long input)
+    {
+        return 0L;
+    }
+
+    /// <summary>
+    ///     The rømer to rømer conversion.
+    /// </summary>
+    /// <param name="input"> The temperature to convert. </param>
+    /// <returns>
+    ///     The converted temperature.
+    /// </returns>
+    public static long RømerToRømer(long input)
+    {
+        return input;
+    }
+}
