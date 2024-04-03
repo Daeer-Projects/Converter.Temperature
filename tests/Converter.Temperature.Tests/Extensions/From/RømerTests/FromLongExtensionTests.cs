008f7c03e56cb2d6f04518a5ba3efe44eb4483c7
David Clark <krennic51@gmail.com>
2024-04-03 15:05:19 +0100

    Add `From` tests for 'Rømer`

---
+++ tests/Converter.Temperature.Tests/Extensions/From/RømerTests/FromLongExtensionTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Converter.Temperature.Tests.Extensions.From.RømerTests;
+
+using FluentAssertions;
+using Temperature.Extensions.From;
+using Types.Rømer;
+using Xunit;
+
+public sealed class FromLongExtensionTests : BaseFromExtensionTests<long, RømerLong>
+{
+    public FromLongExtensionTests() : base(
+        long.MaxValue,
+        999,
+        0,
+        -999,
+        long.MinValue) { }
+
+    protected override RømerLong ConvertFrom(
+        long value)
+    {
+        return value.FromRømer();
+    }
+
+    [Fact]
+    public void Test_from_rømer_returns_rømer_long_type()
+    {
+        // Arrange.
+        const long input = 39L;
+
+        // Act.
+        RømerLong result = input.FromRømer();
+
+        // Assert.
+        result.Should()
+            .BeOfType<RømerLong>()
+            .Which.Temperature.Should()
+            .Be(input);
+    }
+}
---
+++ tests/Converter.Temperature.Tests/Extensions/From/RømerTests/FromStringExtensionTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Converter.Temperature.Tests.Extensions.From.RømerTests;
+
+using System.Globalization;
+using FluentAssertions;
+using Temperature.Extensions.From;
+using Types.Rømer;
+using Xunit;
+
+public sealed class FromStringExtensionTests : BaseFromExtensionTests<string, RømerString>
+{
+    public FromStringExtensionTests() : base(
+        double.MaxValue.ToString(CultureInfo.InvariantCulture),
+        "999.999",
+        "0",
+        "-999.999",
+        double.MinValue.ToString(CultureInfo.InvariantCulture)) { }
+
+    protected override RømerString ConvertFrom(
+        string value)
+    {
+        return value.FromRømer();
+    }
+
+    [Fact]
+    public void Test_from_rømer_returns_rømer_string_type()
+    {
+        // Arrange.
+        const string input = "39";
+
+        // Act.
+        RømerString result = input.FromRømer();
+
+        // Assert.
+        result.Should()
+            .BeOfType<RømerString>()
+            .Which.Temperature.Should()
+            .Be(input);
+    }
+}
