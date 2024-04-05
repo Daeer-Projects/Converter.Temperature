233d11ebb1c383c4d789cdb26f065042f680de0c
David Clark <krennic51@gmail.com>
2024-04-05 15:29:40 +0100

    Add double integration tests

--- tests/Converter.Temperature.Integration.Tests/DoubleTests/ToGasDoubleTests.cs
+++ tests/Converter.Temperature.Integration.Tests/DoubleTests/ToGasDoubleTests.cs
@@ -439,4 +439,150 @@ public sealed class ToGasDoubleTests
     }
 
     #endregion From Rankine
+
+    #region From Rømer
+
+    [Theory]
+    [InlineData(49.5d, 0.25d)]
+    [InlineData(67.35d, 0.25d)]
+    [InlineData(67.875d, 0.5d)]
+    [InlineData(77.85d, 0.5d)]
+    [InlineData(78.375d, 1d)]
+    [InlineData(83.1d, 1d)]
+    [InlineData(83.625d, 2d)]
+    [InlineData(88.35d, 2d)]
+    [InlineData(88.875d, 3d)]
+    [InlineData(98.85d, 3d)]
+    [InlineData(99.375d, 4d)]
+    [InlineData(104.1d, 4d)]
+    [InlineData(104.63d, 5d)]
+    [InlineData(109.35d, 5d)]
+    [InlineData(109.88d, 6d)]
+    [InlineData(117.23d, 6d)]
+    [InlineData(117.75d, 7d)]
+    [InlineData(125.1d, 7d)]
+    [InlineData(125.63d, 8d)]
+    [InlineData(130.35d, 8d)]
+    [InlineData(130.88d, 9d)]
+    [InlineData(135.6d, 9d)]
+    [InlineData(136.13d, 10d)]
+    [InlineData(148.73d, 10d)]
+    public void Test_double_extensions_from_rømer_to_gas_returns_correct_gas_value(
+        double input,
+        double expected)
+    {
+        // Arrange.
+        // Act.
+        double result = input.FromRømer()
+            .ToGas();
+
+        // Assert.
+        result.Should()
+            .Be(expected);
+    }
+
+    [Theory]
+    [InlineData(49.5d, 0.25d)]
+    [InlineData(67.35d, 0.25d)]
+    [InlineData(67.875d, 0.5d)]
+    [InlineData(77.85d, 0.5d)]
+    [InlineData(78.375d, 1d)]
+    [InlineData(83.1d, 1d)]
+    [InlineData(83.625d, 2d)]
+    [InlineData(88.35d, 2d)]
+    [InlineData(88.875d, 3d)]
+    [InlineData(98.85d, 3d)]
+    [InlineData(99.375d, 4d)]
+    [InlineData(104.1d, 4d)]
+    [InlineData(104.63d, 5d)]
+    [InlineData(109.35d, 5d)]
+    [InlineData(109.88d, 6d)]
+    [InlineData(117.23d, 6d)]
+    [InlineData(117.75d, 7d)]
+    [InlineData(125.1d, 7d)]
+    [InlineData(125.63d, 8d)]
+    [InlineData(130.35d, 8d)]
+    [InlineData(130.88d, 9d)]
+    [InlineData(135.6d, 9d)]
+    [InlineData(136.13d, 10d)]
+    [InlineData(148.73d, 10d)]
+    public void Test_double_extensions_generic_from_rømer_to_gas_returns_correct_gas_value(
+        double input,
+        double expected)
+    {
+        // Arrange.
+        // Act.
+        double result = input.From<Rømer>()
+            .To<Gas>();
+
+        // Assert.
+        result.Should()
+            .Be(expected);
+    }
+
+    [Fact]
+    public void Test_double_extensions_from_rømer_to_gas_with_low_invalid_value_throws_exception()
+    {
+        // Arrange.
+        const double input = 48.975d;
+
+        // Act.
+        ArgumentOutOfRangeException result = Assert.Throws<ArgumentOutOfRangeException>(
+            () => input.FromRømer()
+                .ToGas());
+
+        // Assert.
+        result.Message.Should()
+            .Contain("Temp too low for gas mark!");
+    }
+
+    [Fact]
+    public void Test_double_extensions_generic_from_rømer_to_gas_with_low_invalid_value_throws_exception()
+    {
+        // Arrange.
+        const double input = 48.975d;
+
+        // Act.
+        ArgumentOutOfRangeException result = Assert.Throws<ArgumentOutOfRangeException>(
+            () => input.From<Rømer>()
+                .To<Gas>());
+
+        // Assert.
+        result.Message.Should()
+            .Contain("Temp too low for gas mark!");
+    }
+
+    [Fact]
+    public void Test_double_extensions_from_rømer_to_gas_with_high_invalid_value_throws_exception()
+    {
+        // Arrange.
+        const double input = 149.25d;
+
+        // Act.
+        ArgumentOutOfRangeException result = Assert.Throws<ArgumentOutOfRangeException>(
+            () => input.FromRømer()
+                .ToGas());
+
+        // Assert.
+        result.Message.Should()
+            .Contain("Temp too high for gas mark!");
+    }
+
+    [Fact]
+    public void Test_double_extensions_generic_from_rømer_to_gas_with_high_invalid_value_throws_exception()
+    {
+        // Arrange.
+        const double input = 149.25d;
+
+        // Act.
+        ArgumentOutOfRangeException result = Assert.Throws<ArgumentOutOfRangeException>(
+            () => input.From<Rømer>()
+                .To<Gas>());
+
+        // Assert.
+        result.Message.Should()
+            .Contain("Temp too high for gas mark!");
+    }
+
+    #endregion From Rømer
 }
