ae6d7126f108081d0ab112c6fc45fd3b290ac427
David Clark <krennic51@gmail.com>
2026-03-26 14:40:59 +0000

    Add missing To extensions in Celsius folder

--- src/Converter.Temperature/Extensions/To/Celsius/ToStringExtensions.cs
+++ src/Converter.Temperature/Extensions/To/Celsius/ToStringExtensions.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using Converter.Temperature.Converters.Celsius;
+using Converter.Temperature.Converters.Delisle;
 using Converter.Temperature.Converters.Fahrenheit;
 using Converter.Temperature.Converters.Gas;
 using Converter.Temperature.Converters.Kelvin;
+using Converter.Temperature.Converters.Newton;
 using Converter.Temperature.Converters.Rankine;
+using Converter.Temperature.Converters.Réaumur;
 using Converter.Temperature.Converters.Rømer;
 using Converter.Temperature.Extensions.To.Helpers;
 using Converter.Temperature.Types.Celsius;
+using Converter.Temperature.Types.Delisle;
 using Converter.Temperature.Types.Fahrenheit;
 using Converter.Temperature.Types.Gas;
 using Converter.Temperature.Types.Kelvin;
+using Converter.Temperature.Types.Newton;
 using Converter.Temperature.Types.Rankine;
+using Converter.Temperature.Types.Réaumur;
 using Converter.Temperature.Types.Rømer;
 
 namespace Converter.Temperature.Extensions.To.Celsius;
@@ -118,5 +124,53 @@ public static class ToStringExtensions
         int fractionalCount = -1)
     {
         return ToExtensionHelpers.Parser(input.Temperature, RømerDoubleConversions.RømerToCelsius, fractionalCount);
+    }
+
+    /// <summary>
+    ///     Converts the Newton <paramref name="input" /> to Celsius
+    /// </summary>
+    /// <param name="input"> The value to be converted. </param>
+    /// <param name="fractionalCount"> The count of fractional after the decimal point. </param>
+    /// <exception cref="ArgumentOutOfRangeException"> If fractional count is greater than 15. </exception>
+    /// <returns>
+    ///     The Celsius string result.
+    /// </returns>
+    public static string ToCelsius(
+        this NewtonString input,
+        int fractionalCount = -1)
+    {
+        return ToExtensionHelpers.Parser(input.Temperature, NewtonDoubleConversions.NewtonToCelsius, fractionalCount);
+    }
+
+    /// <summary>
+    ///     Converts the Delisle <paramref name="input" /> to Celsius
+    /// </summary>
+    /// <param name="input"> The value to be converted. </param>
+    /// <param name="fractionalCount"> The count of fractional after the decimal point. </param>
+    /// <exception cref="ArgumentOutOfRangeException"> If fractional count is greater than 15. </exception>
+    /// <returns>
+    ///     The Celsius string result.
+    /// </returns>
+    public static string ToCelsius(
+        this DelisleString input,
+        int fractionalCount = -1)
+    {
+        return ToExtensionHelpers.Parser(input.Temperature, DelisleDoubleConversions.DelisleToCelsius, fractionalCount);
+    }
+
+    /// <summary>
+    ///     Converts the Réaumur <paramref name="input" /> to Celsius
+    /// </summary>
+    /// <param name="input"> The value to be converted. </param>
+    /// <param name="fractionalCount"> The count of fractional after the decimal point. </param>
+    /// <exception cref="ArgumentOutOfRangeException"> If fractional count is greater than 15. </exception>
+    /// <returns>
+    ///     The Celsius string result.
+    /// </returns>
+    public static string ToCelsius(
+        this RéaumurString input,
+        int fractionalCount = -1)
+    {
+        return ToExtensionHelpers.Parser(input.Temperature, RéaumurDoubleConversions.RéaumurToCelsius, fractionalCount);
     }
 }
