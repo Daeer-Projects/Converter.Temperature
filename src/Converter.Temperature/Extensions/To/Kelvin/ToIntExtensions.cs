b034dc9d5f1b7439440e9b46e00a97742100ac84
David Clark <krennic51@gmail.com>
2026-03-26 15:19:30 +0000

    Add missing `To` extensions for Kelvin

--- src/Converter.Temperature/Extensions/To/Kelvin/ToIntExtensions.cs
+++ src/Converter.Temperature/Extensions/To/Kelvin/ToIntExtensions.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using Converter.Temperature.Converters.Celsius;
+using Converter.Temperature.Converters.Delisle;
 using Converter.Temperature.Converters.Fahrenheit;
 using Converter.Temperature.Converters.Gas;
 using Converter.Temperature.Converters.Kelvin;
+using Converter.Temperature.Converters.Newton;
 using Converter.Temperature.Converters.Rankine;
+using Converter.Temperature.Converters.Réaumur;
 using Converter.Temperature.Converters.Rømer;
 using Converter.Temperature.Types.Celsius;
+using Converter.Temperature.Types.Delisle;
 using Converter.Temperature.Types.Fahrenheit;
 using Converter.Temperature.Types.Gas;
 using Converter.Temperature.Types.Kelvin;
+using Converter.Temperature.Types.Newton;
 using Converter.Temperature.Types.Rankine;
+using Converter.Temperature.Types.Réaumur;
 using Converter.Temperature.Types.Rømer;
 
 namespace Converter.Temperature.Extensions.To.Kelvin;
@@ -98,5 +104,47 @@ public static class ToIntExtensions
         this RømerInt input)
     {
         return RømerIntConversions.RømerToKelvin(input.Temperature);
+    }
+
+    /// <summary>
+    ///     Converts the Newton <paramref name="input" /> to Kelvin
+    /// </summary>
+    /// <param name="input"> The value to be converted. </param>
+    /// <exception cref="ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+    /// <returns>
+    ///     The Kelvin <see langword="int" /> result.
+    /// </returns>
+    public static int ToKelvin(
+        this NewtonInt input)
+    {
+        return NewtonIntConversions.NewtonToKelvin(input.Temperature);
+    }
+
+    /// <summary>
+    ///     Converts the Delisle <paramref name="input" /> to Kelvin
+    /// </summary>
+    /// <param name="input"> The value to be converted. </param>
+    /// <exception cref="ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+    /// <returns>
+    ///     The Kelvin <see langword="int" /> result.
+    /// </returns>
+    public static int ToKelvin(
+        this DelisleInt input)
+    {
+        return DelisleIntConversions.DelisleToKelvin(input.Temperature);
+    }
+
+    /// <summary>
+    ///     Converts the Réaumur <paramref name="input" /> to Kelvin
+    /// </summary>
+    /// <param name="input"> The value to be converted. </param>
+    /// <exception cref="ArgumentOutOfRangeException"> If calculated value is beyond the limits of the type. </exception>
+    /// <returns>
+    ///     The Kelvin <see langword="int" /> result.
+    /// </returns>
+    public static int ToKelvin(
+        this RéaumurInt input)
+    {
+        return RéaumurIntConversions.RéaumurToKelvin(input.Temperature);
     }
 }
